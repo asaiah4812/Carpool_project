81147c86f8f6f58109504dfa605687caecc3f77d
Shagbaor Agber <dxtlive@gmail.com>
2024-12-27 00:53:58 +0100

    Refine welcome message in DisplayUser component and add React version override in package.json

--- components/DisplayUser.tsx
+++ components/DisplayUser.tsx
@@ -8,7 +8,7 @@ export default function DisplayUser() {
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
-			<h1>Welcome to SkillSwap a Web3-based peer-to-peer learning marketplace focusing on blockchain and decentralized technology skills. </h1>
+			<h1>SkillSwap a Web3-based peer-to-peer learning marketplace focusing on blockchain and decentralized technology skills. </h1>
 			<img src="/img/logo.png" alt=""  width="150"/>
 			<h1 className="text-4xl font-bold">
 				Welcome to the Telegram Web App!
